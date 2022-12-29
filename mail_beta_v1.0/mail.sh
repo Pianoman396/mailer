5f990f0a42a477a86619216645e18688f765e2fc
Eric <playlivepiano@outlook.com>
2022-12-29 07:42:18 +0400

    Update mail.sh

--- mail_beta_v1.0/mail.sh
+++ mail_beta_v1.0/mail.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-urls="webprojecthr@gmail.com career@mgplab.com hr@softcode.am talents@codics.am hr@smart-corner.org zoom@zoom.am hr@codecraft.am hr@velvet.am cretrixllc@gmail.com hr@erasofts.com job@beeoncode.com surveytimehr@gmail.com hr.softler@gmail.com info@novembit.com info@ayotech.am info@beewebsystems.com jobs@sflpro.com hr@lanar.am info@reload.am"  # add more URLs here
+urls=""  # add more URLs with 'space' separation here
 
 for url in $urls; do
    echo "fetching $url"
